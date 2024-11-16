eddb0d593a184bd2feaf90dd9256b30a51e7162f
Viniciusx0800 <vinioliveira2008y@gmail.com>
2024-11-16 00:23:09 -0300

    feat: Concluir desafio

--- desafio01-calculadora/src/App.js
+++ desafio01-calculadora/src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import Input from './components/Input';
+import Button from './components/Button';
+import { Container, Content, Row } from './style';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
+
+  const handleOnClear = () => {
+    setCurrentNumber('0');
+    setFirstNumber('0');
+    setOperation('');
+  };
+
+  const handleAddNumber = (num) => {
+    setCurrentNumber((prev) => (prev === '0' ? num : prev + num));
+  };
+
+  const handleOperation = (op) => {
+    if (firstNumber === '0') {
+      setFirstNumber(currentNumber);
+      setCurrentNumber('0');
+      setOperation(op);
+    } else {
+      // Caso uma operação já tenha sido feita, realiza o cálculo antes de começar outra operação
+      handleEquals();
+      setOperation(op);
+    }
+  };
+
+  const handleEquals = () => {
+    if (firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
+      let result;
+      switch (operation) {
+        case '+':
+          result = Number(firstNumber) + Number(currentNumber);
+          break;
+        case '-':
+          result = Number(firstNumber) - Number(currentNumber);
+          break;
+        case '*':
+          result = Number(firstNumber) * Number(currentNumber);
+          break;
+        case '/':
+          result = Number(firstNumber) / Number(currentNumber);
+          break;
+        default:
+          return;
+      }
+      setCurrentNumber(String(result));
+      setFirstNumber('0');
+      setOperation('');
+    }
+  };
+
+  const handleDecimal = () => {
+    if (!currentNumber.includes('.')) {
+      setCurrentNumber((prev) => prev + '.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+        <Input value={currentNumber} />
+        <Row>
+          <Button label="x" onClick={() => handleOperation('*')} />
+          <Button label="/" onClick={() => handleOperation('/')} />
+          <Button label="c" onClick={handleOnClear} />
+          <Button label="." onClick={handleDecimal} />
+        </Row>
+        <Row>
+          <Button label="7" onClick={() => handleAddNumber('7')} />
+          <Button label="8" onClick={() => handleAddNumber('8')} />
+          <Button label="9" onClick={() => handleAddNumber('9')} />
+          <Button label="-" onClick={() => handleOperation('-')} />
+        </Row>
+        <Row>
+          <Button label="4" onClick={() => handleAddNumber('4')} />
+          <Button label="5" onClick={() => handleAddNumber('5')} />
+          <Button label="6" onClick={() => handleAddNumber('6')} />
+          <Button label="+" onClick={() => handleOperation('+')} />
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => handleAddNumber('1')} />
+          <Button label="2" onClick={() => handleAddNumber('2')} />
+          <Button label="3" onClick={() => handleAddNumber('3')} />
+          <Button label="=" onClick={handleEquals} />
+        </Row>
+        <Row>
+          <Button label="0" onClick={() => handleAddNumber('0')} />
+        </Row>
+      </Content>
+    </Container>
   );
-}
+};
 
 export default App;
